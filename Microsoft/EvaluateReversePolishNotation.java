8d52b9d801ac6185d994acacf286f2e2c8df35c8
Nayan Kumar Jha <121750579+jhaNayan2509@users.noreply.github.com>
2023-01-02 00:10:46 +0530

    Add files via upload

---
+++ Microsoft/EvaluateReversePolishNotation.java
@@ -0,0 +1,30 @@
+/*Problem link: https://leetcode.com/problems/evaluate-reverse-polish-notation/description/ */
+
+class Solution {
+    public int evalRPN(String[] tokens) {
+        Stack<Integer> stack=new Stack();
+        String oprtr="+-*/";
+        for(String s:tokens)
+        {
+            if(oprtr.contains(s)&&!stack.isEmpty()){
+              int op1=stack.pop();
+              int op2=stack.pop();
+              int ans=help(op2,s,op1);
+              stack.push(ans);
+            }else{
+                stack.push(Integer.parseInt(s));
+            }
+        }
+        return stack.pop();
+
+    }
+    public int help(int op2,String s,int op1)
+    {
+        if(s.equals("+"))return op2+op1;
+        else if(s.equals("-"))return op2-op1;
+        else if(s.equals("/"))return op2/op1;
+        return op2*op1;
+    }
+}
+
+/*Submission Link: https://leetcode.com/problems/evaluate-reverse-polish-notation/submissions/869126979/ */
